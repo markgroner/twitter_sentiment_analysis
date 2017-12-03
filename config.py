cc54c7efb43f88df59025c69d73774e36da7817c
Mark Groner <markgroner@utexas.edu>
2017-12-02 18:47:30 -0600

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-twitter_keys = {'consumer_key': 'xto5N5aUO7bnBjupXU9NaLhOv',
-                'consumer_secret': '8JcHWolbKrsMaTwiJs1jOTSn0cPUk4KaqOmf2wTbnc7vmaHdq5',
-                'access_token': '930594407237242882-K9FZus74WQkIXXHyGygrNMbde4weRQc',
-                'access_token_secret': 'dvBZQv9xLS6tdvn9U00xR8wy4L37FvpQHGlM7Hp3MoC9i'
-                }
